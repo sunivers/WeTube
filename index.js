47d2b6e2470b8dc6ec79dd428ad6780ea4b38c96
soyoung <soyoungp02@gmail.com>
2020-05-05 17:11:25 +0900

    install & set middlewares

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+
 const app = express();
 
 const PORT = 4000;
@@ -11,12 +16,11 @@ const handleHome = (req, res) => res.send('Hello from Home!');
 
 const handleProfile = (req, res) => res.send('You are on my profile');
 
-const betweenHome = (req, res, next) => {
-  console.log('I am between');
-  next();
-}
-
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get('/', handleHome);
 
