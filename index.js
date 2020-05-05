3d2641fefc5423c8cc4fb3bf43131e8d0c6e7f2d
soyoung <soyoungp02@gmail.com>
2020-05-05 16:12:19 +0900

    set middleware

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const handleHome = (req, res) => res.send('Hello from Home!');
 
 const handleProfile = (req, res) => res.send('You are on my profile');
 
+const betweenHome = (req, res, next) => {
+  console.log('I am between');
+  next();
+}
+
+app.use(betweenHome);
+
 app.get('/', handleHome);
 
 app.get('/profile', handleProfile);
