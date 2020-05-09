b9f535dd8339bc68a4ffc3c08202c1e5258ddabf
soyoung <soyoungp02@gmail.com>
2020-05-09 18:10:40 +0900

    change entry file

--- app.js
+++ app.js
@@ -6,12 +6,6 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
-const PORT = 4000;
-
-const handleListening = () => {
-  console.log(`Listening on: http://localhost:${PORT}`);
-}
-
 const handleHome = (req, res) => res.send('Hello from Home!');
 
 const handleProfile = (req, res) => res.send('You are on my profile');
@@ -26,4 +20,4 @@ app.get('/', handleHome);
 
 app.get('/profile', handleProfile);
 
-app.listen(PORT, handleListening);
+export default app;
