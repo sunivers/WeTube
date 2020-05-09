bce910c279fad0bb9ec2db796d81badc4f7d385f
soyoung <soyoungp02@gmail.com>
2020-05-10 01:13:39 +0900

    set pug and add view files

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
-export const join = (req, res) => res.send('join');
-export const login = (req, res) => res.send('login');
-export const logout = (req, res) => res.send('logout');
-export const users = (req, res) => res.send("users");
-export const editProfile = (req, res) => res.send('user edit');
-export const changePassword = (req, res) => res.send('user password');
-export const userDetail = (req, res) => res.send("user detail");
+export const join = (req, res) => res.render('join');
+export const login = (req, res) => res.render('login');
+export const logout = (req, res) => res.render('logout');
+export const users = (req, res) => res.render("users");
+export const editProfile = (req, res) => res.render('editProfile');
+export const changePassword = (req, res) => res.render('changePassword');
+export const userDetail = (req, res) => res.render("userDetail");
