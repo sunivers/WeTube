ad90f05f5c4f068851b4b17e852f0024badf809a
soyoung <soyoungp02@gmail.com>
2020-05-19 22:24:04 +0900

    add fake db and home controller

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 324393,
+    title: 'Video awesome',
+    descriptions: 'This is something I love',
+    views: 24,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 121212,
+      name: 'Nocolas',
+      email: 'nico@las.com',
+    }
+  },
+  {
+    id: 493792,
+    title: 'Video perfect',
+    descriptions: 'This is something I love',
+    views: 24,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 121212,
+      name: 'Nocolas',
+      email: 'nico@las.com',
+    }
+  },
+  {
+    id: 87392,
+    title: 'Video super',
+    descriptions: 'This is something I love',
+    views: 24,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 121212,
+      name: 'Nocolas',
+      email: 'nico@las.com',
+    }
+  },
+  {
+    id: 87392,
+    title: 'Video nice',
+    descriptions: 'This is something I love',
+    views: 24,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 121212,
+      name: 'Nocolas',
+      email: 'nico@las.com',
+    }
+  }
+]
