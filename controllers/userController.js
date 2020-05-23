ac5aa110280bf938a1b13aa6ee18e309371cc11c
soyoung <soyoungp02@gmail.com>
2020-05-23 22:47:40 +0900

    Join Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,18 @@
-export const join = (req, res) => res.render('join');
-export const login = (req, res) => res.render('login');
+import routes from "../routes";
+
+export const getJoin = (req, res) => res.render('join', { pageTitle: 'Join' });
+export const postJoin = (req, res) => {
+  const { body: { name, email, password, password2 } } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render('join', { pageTitle: 'Join' });
+  } else {
+    // Todo: Register User
+    // Todo: Log User In
+    res.redirect(routes.home);
+  }
+}
+export const login = (req, res) => res.render('login', { pageTitle: 'Login' });
 export const logout = (req, res) => res.render('logout');
 export const users = (req, res) => res.render("users");
 export const editProfile = (req, res) => res.render('editProfile');
