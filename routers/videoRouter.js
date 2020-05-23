c555cc88cd0e3ddf323b7bb9daee345158924877
soyoung <soyoungp02@gmail.com>
2020-05-24 00:33:40 +0900

    video detail controller

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -6,7 +6,7 @@ const videoRouter = express.Router();
 
 // videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
